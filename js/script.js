c3c312d89ce873222f2e63d3b452198484cb8172
Dmytro Hopko <gopkodimon@gmail.com>
2021-04-04 18:23:16 +0300

    Готовое решения ДЗ Урок 2 final))))

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 let num = 266219;
 let strNum = num.toString().split(''); //преобразование чисел в строку. Строку в масив
-
 console.log(strNum);
 
-let i = strNum.reduce((acc, item) => {
+let result = strNum.reduce((acc, item) => {
     return acc * +item
   }, 1);
+console.log(result);
 
-console.log(i);
+let pow = result ** 3; // возвести в степень 3, используя только 1 оператор (Math.pow не подходит)
+console.log(pow);
+
+pow = pow.toString().substr(0, 2); //Вывести на экран первые 2 цифры полученного числа
+console.log(pow);
