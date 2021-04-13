66c75be5fdad4295988bd6bd59f6adde088f63be
Dmytro Hopko <gopkodimon@gmail.com>
2021-04-13 23:55:51 +0300

    Ready Homework 5

--- js/script.js
+++ js/script.js
@@ -1,70 +1,21 @@
-/* let num = 266219;
-let strNum = num.toString().split(''); //преобразование чисел в строку. Строку в масив
-console.log(strNum);
+"use strict";
 
-let result = strNum.reduce((acc, item) => {
-    return acc * +item
-  }, 1);
-console.log(result);
+// HOMEWORK LESSON 5
+// 5.1
+let arr = ['478544', '25622', '766523', '25896', '45544', '96325', '785320'];
+let str;
 
-let pow = result ** 3; // возвести в степень 3, используя только 1 оператор (Math.pow не подходит)
-console.log(pow);
-
-pow = pow.toString().substr(0, 2); //Вывести на экран первые 2 цифры полученного числа
-console.log(pow);
- */
+for (let i = 0; i < arr.length; i++) {
+  str = arr[i];
+  if (str.substr(0, 1) === '2' || str.substr(0, 1) === '4' ) console.log(arr[i])
+}; 
 
 
-
-// HOMEWOR LESSON 3
-
-let lang = 'ru';  // 'ru' || 'en'
-
-let arr = [
-  arrDaysRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-  arrDaysEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-];
-
-// #1
-// #1.1
-if (lang === 'ru'){
-  console.log(arr[0]);
-} else if (lang === 'en') {
-  console.log(arr[1]);
+// 5.2
+nextPrime:
+for (let i = 2; i <= 100; i++) {
+  for (let j = 2; j < i; j++) {
+    if(i % j == 0) continue nextPrime
+  };  
+  console.log(i + ' Делители этого числа 1 и ' + i )
 };
-
-
-// #1.2
-switch (lang) {
-  case 'ru':
-    console.log(arr[0]);
-    break;
-  case 'en':
-    console.log(arr[1]);
-    break;
-};
-
-
-// #1.3
-let arrResult = lang === 'ru' ? arr[0] : arr[1];
-console.log(arrResult);
-
-
-// #2
-let namePerson = 'Артем' ; // 'Артем', 'Максим' or other value 
-let nameResult = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
-console.log(nameResult)
-
-// HOMEWORK LESSON 4
-console.log('LESSON 4');
-
-let streetName = prompt('Введите название улицы');
-
-function kyiv(street){
-  if(!isNaN(street)) return(street + ' - не является улицей.Пожалуйста введите название улицы');
-  street = street.replace(/\s+/g, '');
-  if(street.length >= 30) return street.substr(0, 30) + '...';
-  return street;
-};
-
-console.log(kyiv(streetName));
