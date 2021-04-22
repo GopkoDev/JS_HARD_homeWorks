34a6e10a66026a732aa4cfc50c8c03a5fdceedc3
Dmytro Hopko <gopkodimon@gmail.com>
2021-04-22 23:22:09 +0300

    ready homework lesson 7

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
 "use strict";
 
-// HOMEWORK LESSON 5
-// 5.1
-let arr = ['478544', '25622', '766523', '25896', '45544', '96325', '785320'];
-let str;
+// HOMEWORK LESSON 7
 
-for (let i = 0; i < arr.length; i++) {
-  str = arr[i];
-  if (str.substr(0, 1) === '2' || str.substr(0, 1) === '4' ) console.log(arr[i])
-}; 
+let date = new Date;
+let week = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday'
+];
 
-
-// 5.2
-nextPrime:
-for (let i = 2; i <= 100; i++) {
-  for (let j = 2; j < i; j++) {
-    if(i % j == 0) continue nextPrime
-  };  
-  console.log(i + ' Делители этого числа 1 и ' + i )
+for (let i = 0; i < week.length; i++ ) {
+  let day = week[i]
+  if (i == 5 || i == 6) day = day.italics();
+  if (i === date.getDay()) day = day.bold();
+  console.log(day);
 };
+
+// for (let item of week) {
+//   console.log(item);
+// };
+
+// Варіант перебору через let in =)
