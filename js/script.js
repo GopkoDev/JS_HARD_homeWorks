f66f718981039afda4f7bdce7de508dceacce2cc
Dmytro Hopko <gopkodimon@gmail.com>
2021-04-24 13:10:18 +0300

    lesson09 homework

--- js/script.js
+++ js/script.js
@@ -1,22 +1,48 @@
 "use strict";
 
-// HOMEWORK LESSON 7
+// HOMEWORK LESSON 9
+function updateTime(){
+	let date = new Date();
+	let d = date;
+	let options = {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		weekday: 'long', 
+		timezone: 'UTC',
+		hour: 'numeric',
+		minute: 'numeric',
+		second: 'numeric'
+	};
 
-let date = new Date;
-let week = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday'
-];
+	let formatDate = function(time) {
+		let hour = time.substr(38, 2);
+		if (hour == 1 || hour == 21) return ' час ' ;
+		if (hour == 2 || hour == 3 || hour == 4 || hour == 22 || hour == 23 || hour == 24) return ' часа ';
+		return ' часов ';
+	};
 
-for (let i = 0; i < week.length; i++ ) {
-  let day = week[i]
-  if (i == 5 || i == 6) day = day.italics();
-  if (i+1 === date.getDay()) day = day.bold();
-  document.write('<div>' + day + '</div>');
+	d = 'Сегодня ' + d.toLocaleString("ru", options);
+	d = d.substr(0, 32) + 'года, ' +d.substr(36, 2) + formatDate(d) + d.substr(39, 2) + ' минут ' + d.substr(42, 2) + ' секунды';
+
+	document.getElementById('date').innerHTML = d;
+
+
+
+	let opt = {
+		year: 'numeric',
+		month: 'numeric',
+		day: 'numeric',
+		timezone: 'UTC',
+		hour: 'numeric',
+		minute: 'numeric',
+		second: 'numeric'
+	};
+
+	date = date.toLocaleString("ru", opt);
+	date = date.substr(0, 10) + ' - ' + date.substr(12) ;
+	document.getElementById('date2').innerHTML = date;
+
 };
 
+setInterval(updateTime, 1000);
