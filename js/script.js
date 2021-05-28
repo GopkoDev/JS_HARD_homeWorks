6b2908fc1d19b525ef7b608ffe5518494d3cac9b
Dmytro Gopko <gopkodimon@gmail.com>
2021-05-28 07:47:36 +0300

    delete old  data and add new

--- js/script.js
+++ js/script.js
@@ -1,38 +1,34 @@
 'use strict'
-const wrapper = document.querySelector('.wrapper')
-const button = document.querySelector('button')
-const h = document.querySelector('h1')
+const bodyHtml = document.querySelector('body')
 
+function DomElement(selector, height, width, bg, fontSize){
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+};
 
-function random (){
-	function getRandomInt(max) {
-		return Math.floor(Math.random() * Math.floor(max));
-	  }
-	  
-	  let red = getRandomInt(255);
-	  let green = getRandomInt(255);
-	  let blue = getRandomInt(255);
-	
-	function RGBToHex(r,g,b) {
-	r = r.toString(16);
-	g = g.toString(16);
-	b = b.toString(16);
-	
-	if (r.length == 1)
-		r = "0" + r;
-	if (g.length == 1)
-		g = "0" + g;
-	if (b.length == 1)
-		b = "0" + b;
-	
-	return "#" + r + g + b;
-	}
-	
-	h.textContent = RGBToHex(red, green, blue)
-	wrapper.style.backgroundColor = RGBToHex(red, green, blue)
-	button.style.color = RGBToHex(red, green, blue)
-}
+DomElement.prototype.divOrId = function(){
+    let newElem = null;
+    if(this.selector.slice(0, 1) === '.') {
+        newElem = document.createElement('div');
+        newElem.classList.add(this.selector.substring(1));
+        newElem.textContent = 'div with class=' + '"' +  this.selector.substring(1) + '"';
+    } else if (this.selector.slice(0, 1) === '#') {
+        newElem = document.createElement('p');
+        newElem.id = this.selector.substring(1);
+        newElem.textContent = 'p with id=' + '"' + this.selector.substring(1) + '"';
+    };
+    newElem.style.cssText = 'height:' + this.height + 
+                        '; width:' + this.width + 
+                        '; background:' + this.bg + 
+                        '; font-size:' + this.fontSize + ';';
+    bodyHtml.prepend(newElem);
+};
 
+//selector, height, width, bg, fontSize
 
-random()
-button.addEventListener('click', random)
+let domElement = new DomElement('.1111', '30px', '100%', 'red', '20px') 
+domElement.divOrId()
+git
