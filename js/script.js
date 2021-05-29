75f19a0340705f524d57961954b4f378c8cc4616
Dmytro Gopko <gopkodimon@gmail.com>
2021-05-29 12:09:28 +0300

    ready HW, moving my square with arrow keys

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 'use strict'
 const bodyHtml = document.querySelector('body')
+let newElem = null,
+    elemTop = 100,
+    elemLeft = 100;
 
 function DomElement(selector, height, width, bg, fontSize){
     this.selector = selector;
@@ -10,25 +13,35 @@ function DomElement(selector, height, width, bg, fontSize){
 };
 
 DomElement.prototype.divOrId = function(){
-    let newElem = null;
-    if(this.selector.slice(0, 1) === '.') {
-        newElem = document.createElement('div');
-        newElem.classList.add(this.selector.substring(1));
-        newElem.textContent = 'div with class=' + '"' +  this.selector.substring(1) + '"';
-    } else if (this.selector.slice(0, 1) === '#') {
-        newElem = document.createElement('p');
-        newElem.id = this.selector.substring(1);
-        newElem.textContent = 'p with id=' + '"' + this.selector.substring(1) + '"';
-    };
+	newElem = document.createElement('div');
+	newElem.classList.add(this.selector.substring(1));
+	newElem.textContent = 'div with class=' + '"' +  this.selector.substring(1) + '"';
     newElem.style.cssText = 'height:' + this.height + 
                         '; width:' + this.width + 
                         '; background:' + this.bg + 
-                        '; font-size:' + this.fontSize + ';';
+                        '; font-size:' + this.fontSize + 
+						'; position: absolute;' +
+                        'top:' + elemTop +
+                        'px; left:' + elemLeft +'px;';
     bodyHtml.prepend(newElem);
 };
 
-//selector, height, width, bg, fontSize
 
-let domElement = new DomElement('.1111', '30px', '100%', 'red', '20px') 
-domElement.divOrId()
-git
+
+//selector, height, width, bg, fontSize
+let domElement = new DomElement('.1111', '110px', '100px', 'black', '10px') 
+
+
+document.addEventListener('DOMContentLoaded', function (){
+	domElement.divOrId()
+})
+
+
+document.addEventListener('keydown', function(event){
+    newElem.remove()
+	if (event.key === 'ArrowUp') elemTop -= 10
+    if (event.key === 'ArrowDown') elemTop += 10
+    if (event.key === 'ArrowLeft') elemLeft -= 10
+    if (event.key === 'ArrowRight') elemLeft += 10
+    domElement.divOrId()
+})
